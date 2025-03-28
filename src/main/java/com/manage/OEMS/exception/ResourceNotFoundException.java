7b72b0ef03dafa341554a25384f68ce5e3504c0c
kusumithasajja <kusumithasajja@gmail.com>
2025-03-28 12:58:14 +0530

    Add files via upload

---
+++ src/main/java/com/manage/OEMS/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.manage.OEMS.exception;
+
+
+
+@SuppressWarnings("serial")
+public class ResourceNotFoundException extends Exception {
+	
+	 public ResourceNotFoundException(String message){
+	        super(message);
+	    }
+
+}
